86b2c28101dfa9c26cf09c1eb0e4071d8b01c936
Emiliano <emiliano.lopez@gmail.com>
2015-09-11 20:18:31 -0300

    archivos para ejercicios

---
+++ ejemplos/u4/charlando.py
@@ -0,0 +1,6 @@
+import saludo
+
+n = input('Ingrese su nombre: ')
+
+saludo.italiano(n)
+saludo.guarani(n)
---
+++ ejemplos/u4/saludo.py
@@ -0,0 +1,20 @@
+def espanol(nom):
+    print('Hola', nom)
+
+def quechua(nom):
+    print('Napaykullayki', nom)
+    
+def italiano(nom):
+    print('Ciao', nom)
+
+def guarani(nom):
+    '''Buen dia, cómo estas?'''
+    print("Mba'éichapa ndepyhareve", nom)
+
+def aymara(nom):
+    '''¿cómo estás?'''
+    print('Kamisaraki', nom)
+    
+def maya(nom):
+    '''¿cómo estás?'''
+    print('Biix yanilech?', nom)
